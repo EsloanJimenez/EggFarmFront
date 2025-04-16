cdfb35d82ee3d56e8f79f94f999b6b267fa2936d
ejpservice <ejptheservice@gmail.com>
2025-04-16 19:22:06 -0400

    URL restored

--- src/utils/apiBaseURL.js
+++ src/utils/apiBaseURL.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'https://eggfarm-cqdrhgdse6engccj.canadacentral-01.azurewebsites.net/';
+export const API_BASE_URL = 'https://eggfarm-cqdrhgdse6engccj.canadacentral-01.azurewebsites.net/api/';
