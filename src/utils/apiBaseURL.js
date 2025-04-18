066df9a698d587fb40ab2e6904a41333d4698414
Esloan Jimenez <118941213+EsloanJimenez@users.noreply.github.com>
2025-04-18 00:23:43 -0400

    Update apiBaseURL.js
    manteniendo el enlace enlocal

--- src/utils/apiBaseURL.js
+++ src/utils/apiBaseURL.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://eggfarm-cqdrhgdse6engccj.canadacentral-01.azurewebsites.net/';
-//export const API_BASE_URL = 'https://localhost:44335/api/';
+//export const API_BASE_URL = 'https://eggfarm-cqdrhgdse6engccj.canadacentral-01.azurewebsites.net/';
+export const API_BASE_URL = 'https://localhost:44335/api/';
